bfc290a756038ac279a46b079caff590083fdaca
Md. Maruf Hossain <69377574+md-maruf-hossain@users.noreply.github.com>
2022-01-05 15:31:37 +0600

    Added players

--- Scalable Smart Contracts using State Channel/scalable_contract.sol
+++ Scalable Smart Contracts using State Channel/scalable_contract.sol
@@ -1,10 +1,27 @@
 pragma solidity 0.8.11;
 
 contract stateChannel {
+
+    address payable public playerOne;
+    address payable public playerTwo;
+    uint256 public escrowOne;
+    uint256 public escrowTwo;
     
-    constructor() public payable {}
-    
-    function exitStateChannel() public {
-        
+    constructor() public payable {
+        require(msg.value > 0);
+
+        playerOne = msg.sender;
+        escrowOne = msg.value;
     }
+    
+    
+    function setupPlayerTwo() public payable {
+        require(msg.sender != playerOne);
+        require(msg.value > 0);
+
+        playerTwo = msg.sender;
+        escrowTwo = msg.value;
+    }
+
+    function exitStateChannel() public {}
 }
