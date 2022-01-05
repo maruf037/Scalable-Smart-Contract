961f3c65471cb7e8296843d8adf5f53bf288b01a
Md. Maruf Hossain <69377574+md-maruf-hossain@users.noreply.github.com>
2022-01-05 15:50:02 +0600

    Added variables to end the game

--- Scalable Smart Contracts using State Channel/scalable_contract.sol
+++ Scalable Smart Contracts using State Channel/scalable_contract.sol
@@ -6,6 +6,18 @@ contract stateChannel {
     address payable public playerTwo;
     uint256 public escrowOne;
     uint256 public escrowTwo;
+
+    //Variables to end the game
+    uint256 public betOne;
+    uint256 public betTwo;
+    uint256 public balanceOne;
+    uint256 public balanceTwo;
+    uint256 public callOne;
+    uint256 public callTwo;
+    bool public isPlayer1BalanceSetUp;
+    bool public isPlayer2BalanceSetUp;
+    uint256 public finalBalanceOne;
+    uint256 public finalBalanceTwo;
     
     constructor() public payable {
         require(msg.value > 0);
