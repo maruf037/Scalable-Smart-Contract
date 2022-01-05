bbf9183e74d7d9c229454b88126e07d8697e7b2c
Md. Maruf Hossain <69377574+md-maruf-hossain@users.noreply.github.com>
2022-01-05 16:18:26 +0600

    Start of recreation of message of player one.

--- Scalable Smart Contracts using State Channel/scalable_contract.sol
+++ Scalable Smart Contracts using State Channel/scalable_contract.sol
@@ -22,7 +22,7 @@ contract stateChannel {
     constructor() public payable {
         require(msg.value > 0);
 
-        playerOne = msg.sender;
+        playerOne = payable(msg.sender);
         escrowOne = msg.value;
     }
     
@@ -31,9 +31,34 @@ contract stateChannel {
         require(msg.sender != playerOne);
         require(msg.value > 0);
 
-        playerTwo = msg.sender;
+        playerTwo = payable(msg.sender);
         escrowTwo = msg.value;
     }
 
-    function exitStateChannel() public {}
+    function exitStateChannel(
+        bytes memory playerMessage,
+        uint256 playerCall,
+        uint256 playerBet,
+        uint256 playerBalance,
+        uint256 playerNonce,
+        uint256 playerSequence,
+        address addressOfMessage) public {
+        
+        require(playerTwo != address(0), '#1 The address
+        of the player is invalid');
+        require(playerMessage.length == 65, '#2 The length
+        of the message is invalid');
+        require(addressOfMessage == playerOne || 
+        addressOfMessage == playerTwo, "#3 You must use a
+        valid address of one of the players');
+
+        uint256 escrowToUse = escrowOne;
+
+        if(addressOfMessage == playerTwo)
+            escrowToUse = escrowTwo;
+
+        //Recreate the signed message for the first player
+        //to verify that the parameters are correct
+        bytes32 message = keccak256(abi.encodePacked)
+    }
 }
